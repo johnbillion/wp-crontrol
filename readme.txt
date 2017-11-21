247891bc2611f6978f6f9773d1e2157636d8dd16
John Blackbourn <johnbillion@gmail.com>
2017-11-21 23:28:47 +0000

    Version 1.6.

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: johnbillion, scompt
 Tags: admin, cron, plugin, control, wp-cron, crontrol
 Requires at least: 4.1
-Tested up to: 4.7
-Stable tag: 1.5
+Tested up to: 4.9
+Stable tag: 1.6
 
 WP Crontrol lets you view and control what's happening in the WP-Cron system.
 
--- wp-crontrol.php
+++ wp-crontrol.php
@@ -5,7 +5,7 @@
  * Description: WP Crontrol lets you view and control what's happening in the WP-Cron system.
  * Author:      John Blackbourn & contributors
  * Author URI:  https://github.com/johnbillion/wp-crontrol/graphs/contributors
- * Version:     1.5
+ * Version:     1.6
  * Text Domain: wp-crontrol
  * Domain Path: /languages/
  * License:     GPL v2 or later
