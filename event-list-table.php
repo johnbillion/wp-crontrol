52fcff7c058fdbac26ea4217c19473c6226bc50a
John Blackbourn <johnbillion@gmail.com>
2019-08-15 03:10:21 +0200

    No need for this to be conditional.

--- event-list-table.php
+++ event-list-table.php
@@ -2,9 +2,7 @@
 
 namespace Crontrol;
 
-if ( ! class_exists( 'WP_List_Table' ) ) {
-	require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
-}
+require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
 
 class Event_List_Table extends \WP_List_Table {
 
