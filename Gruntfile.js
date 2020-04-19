de5cc5936cf2830ede4ac5ac08d340c94494c3b5
John Blackbourn <johnbillion@gmail.com>
2020-04-19 23:00:30 +0100

    Don't need any of this any more.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,40 +4,9 @@ module.exports = function (grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	var pkg = grunt.file.readJSON('package.json');
-	var gig = require('gitignore-globs');
-	var gag = require('gitattributes-globs');
-	var ignored_gitignore = gig('.gitignore', { negate: true } ).map(function(value) {
-		return value.replace(/^!\//,'!');
-	});
-	var ignored_gitattributes = gag( '.gitattributes', { negate: true } ).map(function(value) {
-		return value.replace(/^!\//,'!');
-	});
 	var config = {};
 
 	config.pkg = pkg;
-
-	config.clean = {
-		main: [
-			'<%= wp_deploy.deploy.options.build_dir %>'
-		]
-	};
-
-	config.copy = {
-		main: {
-			src: [
-				'**',
-				'!.*',
-				'!.git/**',
-				'!<%= wp_deploy.deploy.options.assets_dir %>/**',
-				'!<%= wp_deploy.deploy.options.build_dir %>/**',
-				'!README.md',
-				'!wiki/**',
-				ignored_gitignore,
-				ignored_gitattributes
-			],
-			dest: '<%= wp_deploy.deploy.options.build_dir %>/'
-		}
-	};
 
 	config.version = {
 		main: {
@@ -63,27 +32,6 @@ module.exports = function (grunt) {
 		}
 	};
 
-	config.wp_deploy = {
-		deploy: {
-			options: {
-				deploy_trunk: true,
-				deploy_tag: true,
-				plugin_slug: '<%= pkg.name %>',
-				build_dir: 'build',
-				assets_dir: 'assets-wp-repo'
-			}
-		},
-		assets: {
-			options: {
-				deploy_trunk: false,
-				deploy_tag: false,
-				plugin_slug: '<%= pkg.name %>',
-				build_dir: '<%= wp_deploy.deploy.options.build_dir %>',
-				assets_dir: '<%= wp_deploy.deploy.options.assets_dir %>'
-			}
-		}
-	};
-
 	grunt.initConfig(config);
 
 	grunt.registerTask('bump', function(version) {
@@ -95,19 +43,4 @@ module.exports = function (grunt) {
 			'version::' + version
 		]);
 	});
-
-	grunt.registerTask('build', [
-		'clean',
-		'copy'
-	]);
-
-	grunt.registerTask('deploy', [
-		'build',
-		'wp_deploy'
-	]);
-
-	grunt.registerTask('deploy:assets', [
-		'build',
-		'wp_deploy:assets'
-	]);
 };
