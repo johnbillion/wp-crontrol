67d8f02fcc85e4228881230e01948e8e0d3080d2
John Blackbourn <johnbillion@gmail.com>
2020-04-20 23:52:29 +0100

    Begin introducing a script to insert the changelog into the readme.

---
+++ .github/workflows/github.js
@@ -0,0 +1,35 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+const semver = require('semver');
+
+const filename = process.argv[2] || 'readme.md';
+
+async function run() {
+	const myToken = '';
+	const api = new github.GitHub(myToken);
+
+	const { data: releases } = await api.repos.listReleases( {
+		owner: 'johnbillion',
+		repo: 'wp-crontrol',
+	} );
+
+	let published = releases.filter( release =>
+		! release.draft && ! release.prerelease
+	);
+
+	let sorted = published.sort( ( a, b ) =>
+		semver.rcompare( semver.coerce( a.tag_name ), semver.coerce( b.tag_name ) )
+	);
+
+	let changelog = sorted.reduce( ( changelog, release ) =>
+		`${changelog}
+
+### ${release.tag_name} ###
+
+${release.body}`
+	, '## Changelog ##' );
+
+	console.log(changelog);
+}
+
+run();
