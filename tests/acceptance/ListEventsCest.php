dfd7c12732f2c4d1496ad32e9fbad3425a1f01b9
John Blackbourn <john@humanmade.com>
2021-11-20 20:02:58 +0000

    Reimplement the List Events test.

---
+++ tests/acceptance/ListEventsCest.php
@@ -0,0 +1,16 @@
+<?php
+
+class ListEventsCest {
+	public function _before( AcceptanceTester $I ) {
+		$I->loginAsAdmin();
+		$I->amOnPluginsPage();
+		$I->activatePlugin( 'wp-crontrol' );
+	}
+
+	public function ListCronEvents( AcceptanceTester $I ) {
+		$I->amOnAdminPage( 'tools.php?page=crontrol_admin_manage_page' );
+		$I->see( 'Cron Events', 'h1' );
+		$I->see( 'Cron Events', '#crontrol-header' );
+		$I->seeElement( 'table.crontrol-events' );
+	}
+}
