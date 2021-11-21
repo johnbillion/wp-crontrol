c141b571e852c205b5adc4bc5d28c6b518efa592
John Blackbourn <john@humanmade.com>
2021-11-21 00:11:49 +0000

    Docker upping and downing.

--- bin/test.sh
+++ bin/test.sh
@@ -14,10 +14,6 @@ WP_CORE_DIR="${PWD}/${WP_ROOT_FOLDER}"
 # Shorthand:
 WP="./vendor/bin/wp --color --path=$WP_CORE_DIR --url=http://$TEST_SITE_WP_DOMAIN"
 
-# Start the PHP server:
-php -S "$TEST_SITE_WP_DOMAIN" -t "$WP_CORE_DIR" -d disable_functions=mail 2>/dev/null &
-PHP_SERVER_PROCESS_ID=$!
-
 # Reset or install the test database:
 $WP db reset --yes
 
@@ -25,8 +21,4 @@ $WP db reset --yes
 $WP core install --title="Example" --admin_user="${TEST_SITE_ADMIN_USERNAME}" --admin_password="${TEST_SITE_ADMIN_PASSWORD}" --admin_email="${TEST_SITE_ADMIN_EMAIL}"
 
 # Run the functional tests:
-./vendor/bin/codecept run --steps "$1" \
-	|| TESTS_EXIT_CODE=$? && kill $PHP_SERVER_PROCESS_ID && exit $TESTS_EXIT_CODE
-
-# Stop the PHP web server:
-kill $PHP_SERVER_PROCESS_ID
+./vendor/bin/codecept run --steps "$1"
