43541ca9025888ba9f10cfc1a5fb195afa1b7f8c
John Blackbourn <john@humanmade.com>
2021-11-21 00:55:36 +0000

    Remove all this jazz.

--- bin/test.sh
+++ bin/test.sh
@@ -4,21 +4,17 @@
 # -o pipefail Produce a failure return code if any command errors
 set -eo pipefail
 
-set -o allexport
-source ./tests/.env
-set +o allexport
-
 # Specify the directory where the WordPress installation lives:
-WP_CORE_DIR="${PWD}/${WP_ROOT_FOLDER}"
+WP_CORE_DIR="${PWD}/tests/wordpress"
 
 # Shorthand:
-WP="./vendor/bin/wp --color --path=$WP_CORE_DIR --url=http://$TEST_SITE_WP_DOMAIN"
+WP="docker-compose run --rm cli --path=$WP_CORE_DIR --url=http://localhost"
 
 # Reset or install the test database:
 $WP db reset --yes
 
 # Install WordPress:
-$WP core install --title="Example" --admin_user="${TEST_SITE_ADMIN_USERNAME}" --admin_password="${TEST_SITE_ADMIN_PASSWORD}" --admin_email="${TEST_SITE_ADMIN_EMAIL}"
+$WP core install --title="Example" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com"
 
 # Run the functional tests:
 ./vendor/bin/codecept run --steps "$1"
--- tests/wp-config.php
+++ tests/wp-config.php
@@ -3,14 +3,6 @@
  * This is the configuration file that's used for the functional tests and WP-CLI commands.
  */
 
-$_root_dir = dirname( __DIR__ );
-$_env_dir  = __DIR__;
-
-require_once $_root_dir . '/vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable( $_env_dir );
-$dotenv->safeLoad();
-
 // Test with WordPress debug mode (default).
 define( 'WP_DEBUG', true );
 
@@ -19,10 +11,10 @@ define( 'WP_PLUGIN_DIR', dirname( dirname( __DIR__ ) ) );
 // WARNING WARNING WARNING!
 // These tests will DROP ALL TABLES in the database with the prefix named below.
 // DO NOT use a production database or one that is shared with something else.
-define( 'DB_NAME',     getenv( 'TEST_SITE_DB_NAME' ) ?: 'wordpress_test' );
-define( 'DB_USER',     getenv( 'TEST_SITE_DB_USER' ) ?: 'root' );
-define( 'DB_PASSWORD', getenv( 'TEST_SITE_DB_PASSWORD' ) ?: '' );
-define( 'DB_HOST',     getenv( 'TEST_SITE_DB_HOST' ) ?: 'localhost' );
+define( 'DB_NAME',     getenv( 'WORDPRESS_DB_NAME' ) ?: 'wordpress_test' );
+define( 'DB_USER',     getenv( 'WORDPRESS_DB_USER' ) ?: 'root' );
+define( 'DB_PASSWORD', getenv( 'WORDPRESS_DB_PASSWORD' ) ?: '' );
+define( 'DB_HOST',     getenv( 'WORDPRESS_DB_HOST' ) ?: 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
@@ -32,7 +24,7 @@ define( 'DB_COLLATE', '' );
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = getenv( 'TEST_SITE_TABLE_PREFIX' ) ?: 'wp_';
+$table_prefix = 'wp_';
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
