909d3fc2f0f888c0a4651f52549f012095238c4d
John Blackbourn <john@humanmade.com>
2021-11-22 18:54:44 +0000

    Use a dynamic port for the Chrome service too.

--- bin/test.sh
+++ bin/test.sh
@@ -6,6 +6,7 @@ set -eo pipefail
 
 # Shorthand:
 WP_PORT=`docker port wp-crontrol-wordpress | grep "[0-9]+$" -ohE`
+CHROME_PORT=`docker port wp-crontrol-chrome | grep "[0-9]+$" -ohE`
 WP_URL="http://host.docker.internal:${WP_PORT}"
 WP="docker-compose run --rm wpcli wp --url=${WP_URL}"
 
@@ -16,5 +17,6 @@ $WP db reset --yes
 $WP core install --title="Example" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com"
 
 # Run the functional tests:
+TEST_SITE_WEBDRIVER_PORT=$CHROME_PORT \
 TEST_SITE_WP_URL=$WP_URL \
 	./vendor/bin/codecept run acceptance --steps "$1"
