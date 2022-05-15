3756ce53e291976d8d27661c88a5e8687946f95e
John Blackbourn <john@humanmade.com>
2022-05-15 18:04:12 +0200

    Bring the test suite inline with that of Query Monitor.

--- bin/test.sh
+++ bin/test.sh
@@ -4,9 +4,10 @@
 # -o pipefail Produce a failure return code if any command errors
 set -eo pipefail
 
-# Shorthand:
-WP_PORT=`docker port wp-crontrol-wordpress | grep "[0-9]+$" -ohE | head -1`
+# Prep:
+WP_PORT=`docker port wp-crontrol-server | grep "[0-9]+$" -ohE | head -1`
 CHROME_PORT=`docker port wp-crontrol-chrome | grep "[0-9]+$" -ohE | head -1`
+DATABASE_PORT=`docker port wp-crontrol-database | grep "[0-9]+$" -ohE | head -1`
 WP_URL="http://host.docker.internal:${WP_PORT}"
 WP="docker-compose run --rm wpcli wp --url=${WP_URL}"
 
@@ -22,12 +23,13 @@ $WP core install \
 	--admin_password="admin" \
 	--admin_email="admin@example.com" \
 	--skip-email \
-	--exec="mysqli_report( MYSQLI_REPORT_OFF );"
+	--require="wp-content/plugins/wp-crontrol/bin/mysqli_report.php"
 echo "Home URL: $WP_URL"
 $WP plugin activate wp-crontrol
 
 # Run the acceptance tests:
 echo "Running tests..."
 TEST_SITE_WEBDRIVER_PORT=$CHROME_PORT \
-TEST_SITE_WP_URL=$WP_URL \
+	TEST_SITE_DATABASE_PORT=$DATABASE_PORT \
+	TEST_SITE_WP_URL=$WP_URL \
 	./vendor/bin/codecept run acceptance --steps "$1"
