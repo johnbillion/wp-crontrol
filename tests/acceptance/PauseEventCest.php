372d9196b6ef56b10b28b242eca432d2a0bc2f88
John Blackbourn <john@humanmade.com>
2022-08-02 23:30:52 +0100

    Add some more assertions.

--- tests/acceptance/PauseEventCest.php
+++ tests/acceptance/PauseEventCest.php
@@ -20,8 +20,18 @@ class PauseEventCest {
 
 		$I->click( 'Paused events (1)' );
 		$I->see( 'Paused', $row );
+		$I->seeLink( 'Edit', $row );
+		$I->dontSeeLink( 'Run Now', $row );
+		$I->dontSeeLink( 'Pause', $row );
+		$I->seeLink( 'Resume', $row );
+		$I->seeLink( 'Delete', $row );
 
 		$I->click( 'Resume', $row );
 		$I->seeAdminSuccessNotice( 'Resumed the pause_me_soon hook.' );
+		$I->seeLink( 'Edit', $row );
+		$I->seeLink( 'Run Now', $row );
+		$I->seeLink( 'Pause', $row );
+		$I->dontSeeLink( 'Resume', $row );
+		$I->seeLink( 'Delete', $row );
 	}
 }
