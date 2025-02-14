312b21e31f73fe8638ff2dd22c665ee305ccb795
John Blackbourn <john@johnblackbourn.com>
2025-02-14 20:14:05 +0100

    Bump the minimum supported WordPress version. WP Crontrol now supports the six latest versions of WordPress.

--- wp-crontrol.php
+++ wp-crontrol.php
@@ -8,7 +8,7 @@
  * Version:      1.18.0
  * Text Domain:  wp-crontrol
  * Domain Path:  /languages/
- * Requires at least: 5.9
+ * Requires at least: 6.2
  * Requires PHP: 7.4
  * License URI:  https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * License:      GPL v2 or later
