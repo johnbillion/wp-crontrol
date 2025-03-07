3552e527dfc7a4db60f671e4af38643c7407dd34
John Blackbourn <john@johnblackbourn.com>
2025-03-07 20:42:55 +0000

    Add the HTTP method field.

--- js/Event/URLFields.tsx
+++ js/Event/URLFields.tsx
@@ -6,37 +6,54 @@ export default function URLFields({
 	integrityCheck,
 }) {
 	return (
-		<tr>
-			<th scope="row">
-				<label htmlFor="crontrol_url">
-					{ __( 'URL', 'wp-crontrol' ) }
-				</label>
-			</th>
-			<td>
-				{ integrityCheck && (
-					<div className="notice notice-error inline">
-						<p>
-							{ __(
-								'The URL in this event needs to be checked for integrity. This event will not run until you re-save it.',
-								'wp-crontrol'
-							) }
-						</p>
-						<p>
-							<a href="https://wp-crontrol.com/help/check-cron-events/">
-								{ __( 'Read what to do', 'wp-crontrol' ) }
-							</a>
-						</p>
-					</div>
-				) }
-				<input
-					className="regular-text code"
-					defaultValue={ args.url ?? '' }
-					id="crontrol_url"
-					name="crontrol_url"
-					required
-					type="url"
-				/>
-			</td>
-		</tr>
+		<>
+			<tr>
+				<th scope="row">
+					<label htmlFor="crontrol_url">
+						{ __( 'URL', 'wp-crontrol' ) }
+					</label>
+				</th>
+				<td>
+					{ integrityCheck && (
+						<div className="notice notice-error inline">
+							<p>
+								{ __(
+									'The URL in this event needs to be checked for integrity. This event will not run until you re-save it.',
+									'wp-crontrol'
+								) }
+							</p>
+							<p>
+								<a href="https://wp-crontrol.com/help/check-cron-events/">
+									{ __( 'Read what to do', 'wp-crontrol' ) }
+								</a>
+							</p>
+						</div>
+					) }
+					<input
+						className="regular-text code"
+						defaultValue={ args.url ?? '' }
+						id="crontrol_url"
+						name="crontrol_url"
+						required
+						type="url"
+					/>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<label htmlFor="crontrol_method">
+						{ __( 'HTTP Method', 'wp-crontrol' ) }
+					</label>
+				</th>
+				<td>
+					<select id="crontrol_method" name="crontrol_method" defaultValue={ args.method ?? 'GET' }>
+						<option value="GET">GET</option>
+						<option value="POST">POST</option>
+						<option value="HEAD">HEAD</option>
+						<option value="DELETE">DELETE</option>
+					</select>
+				</td>
+			</tr>
+		</>
 	);
 }
