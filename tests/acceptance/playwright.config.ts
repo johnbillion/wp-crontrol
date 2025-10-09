18a7a4cf5565e22e0545122f19b6d1c2141f9eff
John Blackbourn <john@johnblackbourn.com>
2025-10-09 23:15:31 +0100

    Only enforce Chrome on CI.

--- tests/acceptance/playwright.config.ts
+++ tests/acceptance/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 	use: {
 		baseURL,
 		...devices['Desktop Chrome'],
-		channel: 'chrome',
+		channel: process.env.CI ? 'chrome' : undefined,
 		viewport: { width: 1440, height: 900 },
 		trace: 'on-first-retry',
 		screenshot: 'only-on-failure',
