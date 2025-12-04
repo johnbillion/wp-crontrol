580b6f026f7c5788317c29aee6eed747fd5a8219
John Blackbourn <john@johnblackbourn.com>
2025-12-04 22:42:40 +0100

    Add the plugin ID.

--- wp-crontrol.php
+++ wp-crontrol.php
@@ -2,6 +2,7 @@
 /**
  * Plugin Name:  WP Crontrol
  * Plugin URI:   https://wp-crontrol.com
+ * Plugin ID:    did:plc:kjnlj6j6hvasaxc6rchd3pnu
  * Description:  Take control of the cron events on your WordPress website.
  * Author:       John Blackbourn
  * Author URI:   https://wp-crontrol.com
