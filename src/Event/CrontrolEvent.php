f3a2723e5cdb22c37aab56b77ced196a23f07ae6
John Blackbourn <john@johnblackbourn.com>
2025-12-09 19:53:00 +0100

    These methods aren't needed.

--- src/Event/CrontrolEvent.php
+++ src/Event/CrontrolEvent.php
@@ -5,9 +5,6 @@
 
 namespace Crontrol\Event;
 
-use Crontrol\Context\UserContext;
-use Crontrol\Context\FeatureContext;
-
 /**
  * Abstract base class for WP Crontrol managed events.
  *
@@ -23,37 +20,4 @@ abstract class CrontrolEvent extends Event {
 	final public function pausable(): bool {
 		return false;
 	}
-
-	/**
-	 * Check if this event is editable.
-	 *
-	 * Subclasses must implement to check specific capabilities.
-	 *
-	 * @param UserContext $user User capability context.
-	 * @param FeatureContext $features Feature flag context.
-	 */
-	#[\Override]
-	abstract public function editable( UserContext $user, FeatureContext $features ): bool;
-
-	/**
-	 * Check if this event can be run.
-	 *
-	 * Subclasses must implement to check specific capabilities.
-	 *
-	 * @param UserContext $user User capability context.
-	 * @param FeatureContext $features Feature flag context.
-	 */
-	#[\Override]
-	abstract public function runnable( UserContext $user, FeatureContext $features ): bool;
-
-	/**
-	 * Check if this event can be deleted.
-	 *
-	 * Subclasses must implement to check specific capabilities.
-	 *
-	 * @param UserContext $user User capability context.
-	 * @param FeatureContext $features Feature flag context.
-	 */
-	#[\Override]
-	abstract public function deletable( UserContext $user, FeatureContext $features ): bool;
 }
