99006c0b7ea32a2616ddbba0e248fa0b5b8b96df
John Blackbourn <john@johnblackbourn.com>
2026-08-29 12:44:05 +0200

    Bump the minimum supported version of WordPress to 6.6.

--- wp-crontrol.php
+++ wp-crontrol.php
@@ -8,7 +8,7 @@
  * Version:      1.21.1
  * Text Domain:  wp-crontrol
  * Domain Path:  /languages/
- * Requires at least: 6.5
+ * Requires at least: 6.6
  * Requires PHP: 7.4
  * License URI:  https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * License:      GPL v2 or later
